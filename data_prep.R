777aa31e6664e9e6f7f64871bff523567b0cec54
jensculrich <jensj27@gmail.com>
2022-02-10 15:53:44 -0800

    started ssdm but need to thin points first

--- data_prep.R
+++ data_prep.R
@@ -6,6 +6,7 @@ library(sf)
 library(maptools)
 library(geojsonsf)
 library(raster)
+library(spThin)
 
 berry_df1 <- read.csv("./occurrence_data/berry_occurrence.csv")
 berry_df2 <- read.csv("./occurrence_data/berry_occurrence_part2.csv")
@@ -15,7 +16,7 @@ raw_occurrence_df <- rbind(berry_df1, berry_df2)
 ## remove duplicate records
 occurrence_df <- raw_occurrence_df %>%
   distinct(occurrenceID, .keep_all = TRUE) %>% # remove duplicates of the same obseration
-  select(family, genus, species, infraspecificEpithet, taxonRank, # select relevant cols
+  dplyr::select(family, genus, species, infraspecificEpithet, taxonRank, # select relevant cols
          stateProvince, decimalLatitude, decimalLongitude, year, basisOfRecord) %>%
   group_by(species, decimalLatitude, decimalLongitude) %>% 
   distinct(.keep_all = TRUE) %>% # remove duplicates from the exact same lat/long
@@ -53,7 +54,7 @@ str(provinces)
 shape_joined_1 <- st_join(points, provinces, join = st_nearest_feature, maxdist = 10000)
 
 shape_joined_2 <- shape_joined_1 %>%
-  select(-cartodb_id, -draworder, - visibility, -extrude, -tessellate, -nom,
+  dplyr::select(-cartodb_id, -draworder, - visibility, -extrude, -tessellate, -nom,
          -icon, -altitudemode, -X_end, -begin, -timestamp, -show, -X_2012_membership) %>%
   mutate(species = str_replace(species, "×", "")) %>%
   rename("SPECIES" = "species",
@@ -63,7 +64,38 @@ shape_joined_2 <- shape_joined_1 %>%
 out_of_bounds <- read.csv("./Geo_Data/out_of_bound_ranges_provincial.csv")
 sp_distr_province <- anti_join(shape_joined_2, out_of_bounds, by = c('SPECIES', 'PROVINCE'))
 
-write.csv(sp_distr_province, "./occurrence_data/occurrence_data_cleaned.csv")
+# write.csv(sp_distr_province, "./occurrence_data/occurrence_data_cleaned.csv")
+
+### thinning to reduce spatial autocorrelation
+# thin at distance of _ using spThin
+sp_distr_province_df <- read.csv("./occurrence_data/occurrence_data_cleaned.csv")
+
+species <- sp_distr_province_df %>% 
+   distinct(SPECIES, .keep_all = TRUE)
+
+new_df <- data.frame()
+
+for(i in 1:nrow(species)){
+  dat <- sp_distr_province_df %>%
+    filter(SPECIES == "Amelanchier alnifolia") # species[3,i]
+  
+  species_dat <- thin(dat, 
+                      lat.col = "latitude",
+                      long.col = "longitude",
+                      spec.col = "SPECIES",
+                      thin.par = 10,
+                      reps = 2)
+  
+  new_df <- rbind(new_df, species_dat)
+}
+
+sp_distr_province_thinned <- thin(sp_distr_province_df, 
+                                  lat.col = "latitude",
+                                  long.col = "longitude",
+                                  spec.col = "SPECIES",
+                                  thin.par = 10,
+                                  reps = 2)
+
 
 # trim bioclim rasters to Canada
 canada <- st_read("./Geo_Data/canada.geojson", quiet = TRUE) # 1
@@ -82,113 +114,129 @@ points(occurrence_rubus_canadensis$decimalLongitude,
        occurrence_rubus_canadensis$decimalLatitude, 
        col='black', pch=20, cex=0.75)
 
-writeRaster(bioclim1_mask, 
-            "./wc2.1_5m_bio_masked/wc2.1_5m_bio_1.tif")
+# writeRaster(bioclim1_mask, 
+    #        "./wc2.1_5m_bio_masked/wc2.1_5m_bio_1.tif")
 
 bioclim2 <- raster("./wc2.1_5m_bio/wc2.1_5m_bio_2.tif", quiet = TRUE) # 1
 bioclim2_crop <- crop(bioclim2, canada)
 bioclim2_mask <- mask(bioclim2_crop, canada)
-writeRaster(bioclim2_mask, 
-            "./wc2.1_5m_bio_masked/wc2.1_5m_bio_2.tif")
+# writeRaster(bioclim2_mask, 
+      #      "./wc2.1_5m_bio_masked/wc2.1_5m_bio_2.tif")
 
 bioclim3 <- raster("./wc2.1_5m_bio/wc2.1_5m_bio_3.tif", quiet = TRUE) # 1
 bioclim3_crop <- crop(bioclim3, canada)
 bioclim3_mask <- mask(bioclim3_crop, canada)
-writeRaster(bioclim3_mask, 
-            "./wc2.1_5m_bio_masked/wc2.1_5m_bio_3.tif")
+# writeRaster(bioclim3_mask, 
+      #      "./wc2.1_5m_bio_masked/wc2.1_5m_bio_3.tif")
 
 bioclim4 <- raster("./wc2.1_5m_bio/wc2.1_5m_bio_4.tif", quiet = TRUE) # 1
 bioclim4_crop <- crop(bioclim4, canada)
 bioclim4_mask <- mask(bioclim4_crop, canada)
-writeRaster(bioclim4_mask, 
-            "./wc2.1_5m_bio_masked/wc2.1_5m_bio_4.tif")
+# writeRaster(bioclim4_mask, 
+    #        "./wc2.1_5m_bio_masked/wc2.1_5m_bio_4.tif")
 
 bioclim5 <- raster("./wc2.1_5m_bio/wc2.1_5m_bio_5.tif", quiet = TRUE) # 1
 bioclim5_crop <- crop(bioclim5, canada)
 bioclim5_mask <- mask(bioclim5_crop, canada)
-writeRaster(bioclim5_mask, 
-            "./wc2.1_5m_bio_masked/wc2.1_5m_bio_5.tif")
+# writeRaster(bioclim5_mask, 
+      #      "./wc2.1_5m_bio_masked/wc2.1_5m_bio_5.tif")
 
 bioclim6 <- raster("./wc2.1_5m_bio/wc2.1_5m_bio_6.tif", quiet = TRUE) # 1
 bioclim6_crop <- crop(bioclim6, canada)
 bioclim6_mask <- mask(bioclim6_crop, canada)
-writeRaster(bioclim6_mask, 
-            "./wc2.1_5m_bio_masked/wc2.1_5m_bio_6.tif")
+# writeRaster(bioclim6_mask, 
+      #      "./wc2.1_5m_bio_masked/wc2.1_5m_bio_6.tif")
 
 bioclim7 <- raster("./wc2.1_5m_bio/wc2.1_5m_bio_7.tif", quiet = TRUE) # 1
 bioclim7_crop <- crop(bioclim7, canada)
 bioclim7_mask <- mask(bioclim7_crop, canada)
-writeRaster(bioclim7_mask, 
-            "./wc2.1_5m_bio_masked/wc2.1_5m_bio_7.tif")
+# writeRaster(bioclim7_mask, 
+        #    "./wc2.1_5m_bio_masked/wc2.1_5m_bio_7.tif")
 
 bioclim8 <- raster("./wc2.1_5m_bio/wc2.1_5m_bio_8.tif", quiet = TRUE) # 1
 bioclim8_crop <- crop(bioclim8, canada)
 bioclim8_mask <- mask(bioclim8_crop, canada)
-writeRaster(bioclim8_mask, 
-            "./wc2.1_5m_bio_masked/wc2.1_5m_bio_8.tif")
+# writeRaster(bioclim8_mask, 
+     #       "./wc2.1_5m_bio_masked/wc2.1_5m_bio_8.tif")
 
 bioclim9 <- raster("./wc2.1_5m_bio/wc2.1_5m_bio_9.tif", quiet = TRUE) # 1
 bioclim9_crop <- crop(bioclim9, canada)
 bioclim9_mask <- mask(bioclim9_crop, canada)
-writeRaster(bioclim9_mask, 
-            "./wc2.1_5m_bio_masked/wc2.1_5m_bio_9.tif")
+# writeRaster(bioclim9_mask, 
+     #       "./wc2.1_5m_bio_masked/wc2.1_5m_bio_9.tif")
 
 bioclim10 <- raster("./wc2.1_5m_bio/wc2.1_5m_bio_10.tif", quiet = TRUE) # 1
 bioclim10_crop <- crop(bioclim10, canada)
 bioclim10_mask <- mask(bioclim10_crop, canada)
-writeRaster(bioclim10_mask, 
-            "./wc2.1_5m_bio_masked/wc2.1_5m_bio_10.tif")
+# writeRaster(bioclim10_mask, 
+      #      "./wc2.1_5m_bio_masked/wc2.1_5m_bio_10.tif")
 
 bioclim11 <- raster("./wc2.1_5m_bio/wc2.1_5m_bio_11.tif", quiet = TRUE) # 1
 bioclim11_crop <- crop(bioclim11, canada)
 bioclim11_mask <- mask(bioclim11_crop, canada)
-writeRaster(bioclim11_mask, 
-            "./wc2.1_5m_bio_masked/wc2.1_5m_bio_11.tif")
+# writeRaster(bioclim11_mask, 
+       #     "./wc2.1_5m_bio_masked/wc2.1_5m_bio_11.tif")
 
 bioclim12 <- raster("./wc2.1_5m_bio/wc2.1_5m_bio_12.tif", quiet = TRUE) # 1
 bioclim12_crop <- crop(bioclim12, canada)
 bioclim12_mask <- mask(bioclim12_crop, canada)
-writeRaster(bioclim12_mask, 
-            "./wc2.1_5m_bio_masked/wc2.1_5m_bio_12.tif")
+# writeRaster(bioclim12_mask, 
+      #      "./wc2.1_5m_bio_masked/wc2.1_5m_bio_12.tif")
 
 bioclim13 <- raster("./wc2.1_5m_bio/wc2.1_5m_bio_13.tif", quiet = TRUE) # 1
 bioclim13_crop <- crop(bioclim13, canada)
 bioclim13_mask <- mask(bioclim13_crop, canada)
-writeRaster(bioclim13_mask, 
-            "./wc2.1_5m_bio_masked/wc2.1_5m_bio_13.tif")
+# writeRaster(bioclim13_mask, 
+       #     "./wc2.1_5m_bio_masked/wc2.1_5m_bio_13.tif")
 
 bioclim14 <- raster("./wc2.1_5m_bio/wc2.1_5m_bio_14.tif", quiet = TRUE) # 1
 bioclim14_crop <- crop(bioclim14, canada)
 bioclim14_mask <- mask(bioclim14_crop, canada)
-writeRaster(bioclim14_mask, 
-            "./wc2.1_5m_bio_masked/wc2.1_5m_bio_14.tif")
+# writeRaster(bioclim14_mask, 
+      #      "./wc2.1_5m_bio_masked/wc2.1_5m_bio_14.tif")
 
 bioclim15 <- raster("./wc2.1_5m_bio/wc2.1_5m_bio_15.tif", quiet = TRUE) # 1
 bioclim15_crop <- crop(bioclim15, canada)
 bioclim15_mask <- mask(bioclim15_crop, canada)
-writeRaster(bioclim15_mask, 
-            "./wc2.1_5m_bio_masked/wc2.1_5m_bio_15.tif")
+# writeRaster(bioclim15_mask, 
+        #    "./wc2.1_5m_bio_masked/wc2.1_5m_bio_15.tif")
 
 bioclim16 <- raster("./wc2.1_5m_bio/wc2.1_5m_bio_16.tif", quiet = TRUE) # 1
 bioclim16_crop <- crop(bioclim16, canada)
 bioclim16_mask <- mask(bioclim16_crop, canada)
-writeRaster(bioclim16_mask, 
-            "./wc2.1_5m_bio_masked/wc2.1_5m_bio_16.tif")
+# writeRaster(bioclim16_mask, 
+       #     "./wc2.1_5m_bio_masked/wc2.1_5m_bio_16.tif")
 
 bioclim17 <- raster("./wc2.1_5m_bio/wc2.1_5m_bio_17.tif", quiet = TRUE) # 1
 bioclim17_crop <- crop(bioclim17, canada)
 bioclim17_mask <- mask(bioclim17_crop, canada)
-writeRaster(bioclim17_mask, 
-            "./wc2.1_5m_bio_masked/wc2.1_5m_bio_17.tif")
+# writeRaster(bioclim17_mask, 
+        #   "./wc2.1_5m_bio_masked/wc2.1_5m_bio_17.tif")
 
 bioclim18 <- raster("./wc2.1_5m_bio/wc2.1_5m_bio_18.tif", quiet = TRUE) # 1
 bioclim18_crop <- crop(bioclim18, canada)
 bioclim18_mask <- mask(bioclim18_crop, canada)
-writeRaster(bioclim18_mask, 
-            "./wc2.1_5m_bio_masked/wc2.1_5m_bio_18.tif")
+# writeRaster(bioclim18_mask, 
+      #      "./wc2.1_5m_bio_masked/wc2.1_5m_bio_18.tif")
 
 bioclim19 <- raster("./wc2.1_5m_bio/wc2.1_5m_bio_19.tif", quiet = TRUE) # 1
 bioclim19_crop <- crop(bioclim19, canada)
 bioclim19_mask <- mask(bioclim19_crop, canada)
-writeRaster(bioclim19_mask, 
-            "./wc2.1_5m_bio_masked/wc2.1_5m_bio_19.tif")
+# writeRaster(bioclim19_mask, 
+     #       "./wc2.1_5m_bio_masked/wc2.1_5m_bio_19.tif")
+
+biome_sf <- st_read("./geo_data/canada_ecoregions_clipped.geojson", quiet = TRUE) # 1
+
+str(biome_sf)
+# Rasterize biome field and write to disk
+biome <- raster(crs = "+proj=longlat +datum=WGS84", vals = 0, resolution = 0.08333333, 
+                ext = extent(-141, -52.66667, 41.66667, 83.25))  %>%
+  rasterize(biome_sf, .) 
+
+xres(bioclim1_mask)
+xres(biome)
+extent(bioclim1_mask)
+extent(biome)
+
+writeRaster(biome, 
+      "./wc2.1_5m_bio_masked/biome.tif")
--- model_sd.R
+++ model_sd.R
@@ -6,6 +6,8 @@ library(maptools)
 library(dismo)
 library(rJava)
 library(tidyverse)
+data(wrld_simpl)
+library(SSDM)
 
 occurrence_df <- read.csv("./occurrence_data/occurrence_data_cleaned.csv")
 
@@ -28,29 +30,17 @@ bioclim16 <- raster("./wc2.1_5m_bio_masked/wc2.1_5m_bio_16.tif", quiet = TRUE) #
 bioclim17 <- raster("./wc2.1_5m_bio_masked/wc2.1_5m_bio_17.tif", quiet = TRUE) # 1
 bioclim18 <- raster("./wc2.1_5m_bio_masked/wc2.1_5m_bio_18.tif", quiet = TRUE) # 1
 bioclim19 <- raster("./wc2.1_5m_bio_masked/wc2.1_5m_bio_19.tif", quiet = TRUE) # 1
+biome <- raster("./wc2.1_5m_bio_masked/biome.tif", quiet = TRUE)
 
 # can test that everything looks right
 occurrence_df_test <- occurrence_df %>%
-  filter(SPECIES == "Rubus canadensis")
+  filter(SPECIES == "Malus fusca")
 # plot(bioclim1)
 # points(occurrence_df_test$longitude, 
 #        occurrence_df_test$latitude, 
 #        col='black', pch=20, cex=0.75)
 
-canada <- st_read("./Geo_Data/canada.geojson", quiet = TRUE) # 1
-biome_sf <- st_read("./geo_data/canada_ecoregions_clipped.geojson", quiet = TRUE) # 1
-
-str(biome_sf)
-# Rasterize biome field and write to disk
-biome <- raster(crs = "+proj=longlat +datum=WGS84", vals = 0, resolution = 0.08333333, 
-                ext = extent(-141, -52.66667, 41.66667, 83.25))  %>%
-    rasterize(biome_sf, .) 
-biome2 <- raster(ext=extent(41.66667, 83.25, -141, -52.66667), res=c(0.01, 0.01))
-
-xres(bioclim1)
-xres(biome)
-extent(bioclim1)
-extent(biome)
+# canada <- st_read("./Geo_Data/canada.geojson", quiet = TRUE) # 1
 
 predictors <- stack(bioclim1, bioclim2, bioclim3, bioclim4, bioclim5,
                     bioclim6, bioclim7, bioclim8, bioclim9, bioclim10,
@@ -58,9 +48,9 @@ predictors <- stack(bioclim1, bioclim2, bioclim3, bioclim4, bioclim5,
                     bioclim16, bioclim17, bioclim18, bioclim19,
                     biome)
   
-names(predictors)
-plot(biome)
-plot(bioclim1)
+# names(predictors)
+# plot(biome)
+# plot(bioclim1)
 
 # first layer of the RasterStack
 #plot(predictors, 1)
@@ -71,31 +61,51 @@ plot(bioclim1)
 #points(occurrence_df_test$longitude, 
 #       occurrence_df_test$latitude, 
 #       col='black', pch=20, cex=0.75)
+ext <- extent(bioclim1)
 
 occurrence_df_test <- occurrence_df_test[,11:12]
 group <- kfold(occurrence_df_test, 5)
 
-# "layer" is the name of the biomes
-jar <- paste(system.file(package="dismo"), "/java/maxent.jar", sep='')
-if (file.exists(jar)) {
-  xm <- maxent(predictors, pres_train, factor="layer")
-  plot(xm)
-  } else {
-    cat('cannot run this example because maxent is not available')
-    plot(1)
-  }
+pres_train <- occurrence_df_test[group != 1, ]
+pres_test <- occurrence_df_test[group == 1, ]
 
-if (file.exists(jar)) {
-  e <- evaluate(pres_test, backg_test, xm, predictors)
-  e
-  px = predict(predictors, xm, ext=ext, progress='')
-  par(mfrow=c(1,2))
-  plot(px, main='Maxent, raw values')
-  plot(wrld_simpl, add=TRUE, border='dark grey')
-  tr <- threshold(e, 'spec_sens')
-  plot(px > tr, main='presence/absence')
-  plot(wrld_simpl, add=TRUE, border='dark grey')
-  points(pres_train, pch='+')
- } else {
-    plot(1)
-}
+backg <- randomPoints(predictors, n=1000, ext=ext, extf = 1.25)
+colnames(backg) = c('lon', 'lat')
+group <- kfold(backg, 5)
+backg_train <- backg[group != 1, ]
+backg_test <- backg[group == 1, ]
+
+# evaluate importance of predictors for the training points
+xm <- maxent(predictors, pres_train, factor="biome")
+# use  RasterStack with predictor variables to make a prediction to RasterLayer:
+pb <- predict(predictors, xm, ext=ext, progress='')
+
+# evaluate the model in a similar way, by providing presence and 
+# background (absence) points, the model, and a RasterStack:
+e <- dismo::evaluate(pres_test, backg_test, xm, predictors)
+
+# Find a threshold
+tr <- dismo::threshold(e, 'spec_sens')
+tr
+
+# plot raw values and presence/absence from threshold
+par(mfrow=c(1,2), mai = c(1, 0.5, 0.5, 1))
+plot(pb, main='Bioclim, raw values')
+plot(pb > tr, main='presence/absence')
+points(pres_train, pch='+')
+
+### SDM species stacking using 'SSDM' package
+predictors <- predictors
+occurrence_df_ssdm <- occurrence_df %>%
+  dplyr::select(SPECIES, latitude, longitude) %>%
+  rename("LONGITUDE" = "longitude",
+         "LATITUDE" = "latitude") %>%
+  dplyr::relocate(SPECIES, LONGITUDE, LATITUDE)
+
+# can test how many species with data (one or more lat/long)
+# this shows 202 - there are 206 in the berry list
+# species <- occurrence_df_ssdm %>% 
+#   distinct(SPECIES, .keep_all = TRUE)
+
+
+
